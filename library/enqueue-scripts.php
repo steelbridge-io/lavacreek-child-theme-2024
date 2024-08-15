d38b2d29aa2bd4d3c4dd36703417bc8d01360807
steelbridge-io <chris@steelbridge.io>
2024-08-14 23:07:37 -0700

    Update URL schemes for AOS and other scripts to HTTPS
    Switch URLs from HTTP to HTTPS for the AOS and signup form scripts to enhance security. This change ensures all external resources are served over secure connections.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -19,24 +19,24 @@ if ( ! function_exists( 'lavacreek_scripts' ) ) :
 
         // Enqueue the main Stylesheet.
         wp_enqueue_style('main-stylesheet', get_template_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path('app.css'), array(), '2.10.4', 'all');
-								
+
         wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/parent-theme-tfs-custom.css', array(), '1.0.0', 'all');
 
         // Child Theme CSS
         wp_enqueue_style( 'child-style',get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
 
         wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array(), '5.7.2', 'all');
-					
+
         // AOS - Animate On Scroll
-        wp_enqueue_style('aos-css', 'http://unpkg.com/aos@2.3.1/dist/aos.css', array(), '2.3.1', 'all');
+        wp_enqueue_style('aos-css', 'https://unpkg.com/aos@2.3.1/dist/aos.css', array(), '2.3.1', 'all');
 
         // Deregister the jquery version bundled with WordPress.
         wp_deregister_script('jquery');
 
         // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
         wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', true);
-		
-        wp_enqueue_script('aos-js', 'http://unpkg.com/aos@2.3.1/dist/aos.js', array('jquery'), '2.3.1', true);
+
+        wp_enqueue_script('aos-js', 'https://unpkg.com/aos@2.3.1/dist/aos.js', array('jquery'), '2.3.1', true);
         wp_add_inline_script('aos-js', 'jQuery(document).ready(function(){
           AOS.init();
         });', 'after');
@@ -64,22 +64,22 @@ if ( ! function_exists( 'lavacreek_scripts' ) ) :
         if (is_singular() && comments_open() && get_option('thread_comments')) {
             wp_enqueue_script('comment-reply');
         }
-					
+
         //if (basename(get_page_template()) === 'landing-page.php' ) {
-            wp_enqueue_script( 'my_script', 'http://static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js', array(), null, true );
+            wp_enqueue_script( 'my_script', 'https://static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js', array(), null, true );
             wp_add_inline_script( 'my_script', 'var _ctct_m = "0a0f5b541f83f517b80813b9cfbdb8d9";' );
        // }
-	} 
-  endif; 
+	}
+  endif;
 	add_action( 'wp_enqueue_scripts', 'lavacreek_scripts' );
-	
+
 	function enqueue_my_script() {
 	//if ( is_page_template( 'landing-page.php' ) ) {
- 
+
 		?>
 		<!-- Begin Constant Contact Active Forms -->
 		<script> var _ctct_m = "0a0f5b541f83f517b80813b9cfbdb8d9"; </script>
-		<script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+		<script id="signupScript" src="https://static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
 		<!-- End Constant Contact Active Forms -->
 		<?php
 	//}
